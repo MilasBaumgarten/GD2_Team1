f6d5ff8db6cd0e584d5609a32d523589c5bf9064
Unknown <christian-luka.frank@gmx.net>
2018-04-18 17:44:11 +0200

    Projectile Move von Chris

---
+++ Assets/Scripts/Projectile/ProjectileMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+
+    public float speed;
+    public LayerMask layerMask;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, speed, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * speed, Color.white);
+        }
+
+    }
+}
