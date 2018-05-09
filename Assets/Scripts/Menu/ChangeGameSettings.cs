bd0165efd31468c9ec47a758372e97e15d88b1d6
Unknown <christian-luka.frank@gmx.net>
2018-05-09 20:59:11 +0200

    Menue ueberarbeitet
    maxAmmo feld wird ausgegraut bei inf. ammo und alle starteinstellungen werden von den jeweiligen variablen geholt

--- Assets/Scripts/Menu/ChangeGameSettings.cs
+++ Assets/Scripts/Menu/ChangeGameSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 //Dieses Skript ändert die jeweiligen Einstellungsrelevanten Variablen
 public class ChangeGameSettings : MonoBehaviour {
 
@@ -9,6 +10,46 @@ public class ChangeGameSettings : MonoBehaviour {
     public ProjectileScriptableObject projectile;
     public PlayerScriptableObject player;
     public ExplosionScriptableObject explosion;
+
+    public InputField projectileSpeed;
+    public InputField ammo;
+    public InputField fireRate;
+    public InputField jumpForce;
+    public InputField movementSpeed;
+    public InputField explosionForce;
+    public InputField explosionRadius;
+    
+
+
+    //Startwerte für die Settings abfragen
+    private void Start()
+    {
+        projectileSpeed.text = string.Format("{0:N2}", projectile.projectileSpeed);
+        ammo.text = string.Format("{0}", projectile.maxAmmo);
+        fireRate.text = string.Format("{0:N2}", projectile.fireRate);
+        jumpForce.text = string.Format("{0:N2}", player.jumpForce);
+        movementSpeed.text = string.Format("{0:N2}", player.maxMoveSpeed);
+        explosionForce.text = string.Format("{0:N2}", explosion.explosionForce);
+        explosionRadius.text = string.Format("{0:N2}", explosion.radius);
+    }
+
+
+    //keine munitionseingabe möglich machen, unendlich munition aktiviert ist
+    private void Update()
+    {
+        if(projectile.infAmmo == true)
+        {
+            ammo.interactable = false;
+        }
+        else
+        {
+            ammo.interactable = true;
+        }
+    }
+
+    //***********************
+    //*****Werte aendern*****
+    //***********************
 
     //Lautstärke anpassen
     public void setVolume(float value)
