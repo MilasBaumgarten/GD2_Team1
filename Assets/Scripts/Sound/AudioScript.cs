c82f4a5a26f90c1385695b38ba19a5349a922f0b
Unknown <christian-luka.frank@gmx.net>
2018-06-22 15:01:20 +0200

    Für Felix

--- Assets/Scripts/Sound/AudioScript.cs
+++ Assets/Scripts/Sound/AudioScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 
 public class AudioScript : MonoBehaviour {
@@ -9,19 +7,26 @@ public class AudioScript : MonoBehaviour {
 
     public void SetMasterSound(float soundLevel)
     {
-        mixer.SetFloat("masterVol", Mathf.Log(soundLevel) * 20);
+        if(soundLevel != 0)
+        {
+            mixer.SetFloat("masterVol", Mathf.Log(soundLevel,10) * 20);
+        }
+        else
+        {
+            mixer.SetFloat("masterVol", -80);
+        }
     }
     public void SetMusicSound(float soundLevel)
     {
-        mixer.SetFloat("musicVol", Mathf.Log(soundLevel) * 20);
+        mixer.SetFloat("musicVol", Mathf.Log(soundLevel,10) * 20);
     }
     public void SetSFXSound(float soundLevel)
     {
-        mixer.SetFloat("sfxVol", Mathf.Log(soundLevel) * 20);
+        mixer.SetFloat("sfxVol", Mathf.Log(soundLevel,10) * 20);
     }
     public void SetOtherSound(float soundLevel)
     {
-        mixer.SetFloat("otherVol", Mathf.Log(soundLevel) * 20);
+        mixer.SetFloat("otherVol", Mathf.Log(soundLevel,10) * 20);
     }
 
 
