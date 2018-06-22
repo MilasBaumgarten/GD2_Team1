6611641f206f4b01e42a4f07a098f773d1149410
Unknown <christian-luka.frank@gmx.net>
2018-06-22 15:20:02 +0200

    VolumeFix

--- Assets/Scripts/Sound/AudioScript.cs
+++ Assets/Scripts/Sound/AudioScript.cs
@@ -18,15 +18,36 @@ public class AudioScript : MonoBehaviour {
     }
     public void SetMusicSound(float soundLevel)
     {
-        mixer.SetFloat("musicVol", Mathf.Log(soundLevel,10) * 20);
+        if (soundLevel != 0)
+        {
+            mixer.SetFloat("musicVol", Mathf.Log(soundLevel, 10) * 20);
+        }
+        else
+        {
+            mixer.SetFloat("musicVol", -80);
+        }
     }
     public void SetSFXSound(float soundLevel)
     {
-        mixer.SetFloat("sfxVol", Mathf.Log(soundLevel,10) * 20);
+         if (soundLevel != 0)
+         {
+            mixer.SetFloat("sfxVol", Mathf.Log(soundLevel, 10) * 20);
+         }
+         else
+         {
+            mixer.SetFloat("sfxVol", -80);
+         }
     }
     public void SetOtherSound(float soundLevel)
     {
-        mixer.SetFloat("otherVol", Mathf.Log(soundLevel,10) * 20);
+         if (soundLevel != 0)
+         {
+            mixer.SetFloat("otherVol", Mathf.Log(soundLevel, 10) * 20);
+         }
+         else
+         {
+            mixer.SetFloat("otherVol", -80);
+         }
     }
 
 
