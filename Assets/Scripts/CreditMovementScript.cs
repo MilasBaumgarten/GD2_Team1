8a6ad3d0c2ce7b15277e537240b1d879b1217e98
T <gorignack@gmx.de>
2018-07-19 15:10:06 +0200

    Credits laufen durch
    Credits sind verfasst und in Textboxen editierbar. Laufen von Unten nach oben durch. Geschwindigkeit in Speed Variable anpassbar falls benötigt. Sound in Audio Source austauschbar falls neuer Credit Track kommen sollte. Wechsel nach Ende ins Hauptmenü müsste noch kommen sobald Menü existiert.

---
+++ Assets/Scripts/CreditMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditMovementScript : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+
+	[SerializeField]
+	private float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void FixedUpdate () {
+		float vertical = Input.GetAxis ("Vertical");
+
+		HandleMovement ();		
+	}
+
+	private void HandleMovement(){
+		myRigidbody.velocity = Vector2.up * speed;
+	}
+}
