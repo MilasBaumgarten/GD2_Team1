ab4456710c78704f15bea83f60fafec4ffe96a7d
Milas Baumgarten <milas.baumgarten@freenet.de>
2018-08-06 16:54:40 +0200

    Turminneres wird bei Betreten von Triggern geladen

--- Assets/Editor/ImportSettings.cs
+++ Assets/Editor/ImportSettings.cs
@@ -31,30 +31,3 @@ public class ImportSettings : AssetPostprocessor
 		}
 	}
 }
-
-/*
- * void OnPostprocessTexture(Texture2D texture)
-    {
-        // Only post process textures if they are in a folder
-        // "invert color" or a sub folder of it.
-        string lowerCaseAssetPath = assetPath.ToLower();
-        if (lowerCaseAssetPath.IndexOf("/invert color/") == -1)
-            return;
-
-        for (int m = 0; m < texture.mipmapCount; m++)
-        {
-            Color[] c = texture.GetPixels(m);
-
-            for (int i = 0; i < c.Length; i++)
-            {
-                c[i].r = 1 - c[i].r;
-                c[i].g = 1 - c[i].g;
-                c[i].b = 1 - c[i].b;
-            }
-            texture.SetPixels(c, m);
-        }
-        // Instead of setting pixels for each mip map levels, you can also
-        // modify only the pixels in the highest mip level. And then simply use
-        // texture.Apply(true); to generate lower mip levels.
-    }
-*/
---
+++ Assets/Scripts/UnLoadScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class UnLoadScene : MonoBehaviour {
+	[Tooltip("Szenen, die beim Betreten des Triggers geladen bzw entfernt werden sollen")]
+	[SerializeField]
+	private string[] scenes;
+
+	[Tooltip("Trigger, die gleiche Szenen laden oder entfernen (Ladetrigger und Entferntrigger müssen hier zusammen aufgeführt werden)")]
+	[SerializeField]
+	private GameObject[] trigger;
+
+	public bool isUnloader;
+
+	[HideInInspector]
+	public bool loaded = false;
+
+	private void OnTriggerEnter(Collider collider) {
+		// wenn Spieler den Trigger betritt
+		if (collider.tag == "Player") {
+			foreach (string scene in scenes) {
+				if (!loaded && !isUnloader) {
+					// lade alle angegebenen Szenen, falls nicht schon geschehen
+					SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+				} else if (loaded && isUnloader) {
+					// entferne alle angegebenen Szenen, falls diese bereits geladen sind
+					SceneManager.UnloadSceneAsync(scene);
+				}
+			}
+
+			if (scenes.Length > 0) {
+				// vermerke für jeden beteiligten Trigger, dass Szenen geladen bzw entfernt wurden
+				loaded = (isUnloader) ? false : true;
+
+				foreach(GameObject g in trigger) {
+					g.GetComponent<UnLoadScene>().loaded = (isUnloader) ? false : true;
+				}
+			}
+		}
+	}
+}
