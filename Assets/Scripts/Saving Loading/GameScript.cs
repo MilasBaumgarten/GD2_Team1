c236a31723d71838b5d45976f831d08dfb763746
Unknown <christian-luka.frank@gmx.net>
2018-08-12 22:16:11 +0200

    Erster versuch an Serialization, noch nicht fertig!!

---
+++ Assets/Scripts/Saving Loading/GameScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameScript{
+    public static GameScript current;
+    public int currentCheckpoint;
+
+    public GameScript()
+    {
+        currentCheckpoint = -1;
+    }
+}
---
+++ Assets/Scripts/Saving Loading/SaveLoad.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoad : MonoBehaviour {
+
+    public static GameScript savedGame;
+    private static string path = Application.dataPath + "savegame.json";
+
+	public static void OverwriteSave()
+    {
+        savedGame = GameScript.current;
+        string json = JsonUtility.ToJson(savedGame);
+        File.WriteAllText(path, json);
+    }
+
+    public static void NewSave()
+    {
+        savedGame = new GameScript();
+        string json = JsonUtility.ToJson(savedGame);
+        File.WriteAllText(path, json);
+        print(path);
+    }
+
+    public static void Load()
+    {
+        if(File.Exists(Path.Combine(Application.persistentDataPath, "savegame.json")))
+        {
+            string json = File.ReadAllText(path);
+            savedGame = JsonUtility.FromJson<GameScript>(json);
+        }
+    }
+}
+
