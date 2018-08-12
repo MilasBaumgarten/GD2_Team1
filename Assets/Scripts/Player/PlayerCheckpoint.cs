3a58cd1e1bfa332e04fb3952548a769725d4ea59
Unknown <christian-luka.frank@gmx.net>
2018-08-12 22:05:32 +0200

    ObjectMovementScript, kann für türen oder anderes benutzt werden
    außerdem checkpoints + speichern/laden, fehlt die Serialization, kommt genauso wie soundwechsel :/

---
+++ Assets/Scripts/Player/PlayerCheckpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheckpoint : MonoBehaviour {
+
+    protected CheckpointNew currentCheckpoint;
+
+	public void SetCheckpoint(CheckpointNew checkpoint)
+    {
+        if (checkpoint != null)
+            currentCheckpoint = checkpoint;
+    }
+	
+    public void Spawn()
+    {
+        if (currentCheckpoint != null)
+        {
+            transform.position = currentCheckpoint.transform.position;
+            transform.rotation = currentCheckpoint.transform.rotation;
+        }
+        else
+        {
+            Debug.LogError("Kein Checkpoint gesetzt.");
+        }
+            
+    }
+	
+}
