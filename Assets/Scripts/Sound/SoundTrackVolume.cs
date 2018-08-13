fa4650343d300159762503d224817c22d5783f4b
Unknown <christian-luka.frank@gmx.net>
2018-08-13 03:00:18 +0200

    Soundwechsel

---
+++ Assets/Scripts/Sound/SoundTrackVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrackVolume : MonoBehaviour {
+
+    SoundTrack soundTrack;
+
+    void OnEnable()
+    {
+        soundTrack = GetComponentInParent<SoundTrack>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        soundTrack.TrackHinzufuegen(this.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        soundTrack.TrackEntfernen();
+    }
+}
