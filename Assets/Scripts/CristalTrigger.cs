ee41c4e6601ff4d50ca5d0eedacd8e1140b19ac4
Unknown <christian-luka.frank@gmx.net>
2018-08-15 20:24:35 +0200

    KristalScripts & Passende Prefabs

---
+++ Assets/Scripts/CristalTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+public class CristalTrigger : MonoBehaviour {
+
+    public GameObject light;
+    public bool isActive = false;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(!isActive)
+        {
+            light.SetActive(true);
+            Debug.Log("Licht");
+        }
+    }
+
+
+}
